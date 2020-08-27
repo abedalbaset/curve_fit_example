9c1d9e76d451fda7cb1586b488ddcdefade26e49
abedalbaset <abedalbasethamam@gmail.com>
2020-08-28 00:27:42 +0300

    Update index.py

--- index.py
+++ index.py
@@ -11,17 +11,34 @@ import pylab as plb
 x = [1,2,3,4,5]
 y= [1,4,9,16,25]
 
-def func(x, a, b, c):
-  return a*x**b +c*x
+def func(x, a,b,d):
+    # -*- coding: utf-8 -*-
 
-p0 = sy.array([0.1,0.1,0.1])
+    e1=a*x**b
+    e2=d
+    equation=e1+e2
+    return equation
+
+p0 = sy.array([1,1,1])
 coeffs, matcov = curve_fit(func, x, y, p0)
 
 
 print("coeffs ",coeffs)
 print("matcov ",matcov)
 
+arrinputs=[]
+arroutputs=[]
 
-#yaj = func(x, coeffs[0], coeffs[1], coeffs[2])
-#plt.plot(x,y,'x',x,yaj,'r-')
-#plt.show()
+for c in range(0,100):
+    yaj = func(c, coeffs[0], coeffs[1], coeffs[2])
+    arrinputs.append(c)
+    arroutputs.append(yaj)
+
+print("arrinputs ",arrinputs)
+print("arroutputs ",arroutputs)
+#markers_on = [12, 17, 18, 19]
+plt.plot(arrinputs, arroutputs, '-bD', markevery=y)
+
+#plt.plot(x,y,'x',x,'r-')
+#plt.plot(arrinputs,arroutputs)
+plt.show()
